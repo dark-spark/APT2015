9227f23cd047d4a23c9124f1e1b5de00721c256d
dark-spark <matt.syme@gmail.com>
2015-03-15 09:23:41 +1000

    More work, all IO's are set

--- Prop/Prop.ino
+++ Prop/Prop.ino
@@ -1,15 +1,5 @@
 #include <Adafruit_NeoPixel.h>
 
-int floatingPin = A0;
-const int AI0 = A1;
-const int AI1 = A2;
-const int AI2 = A3;
-const int AI3 = A4;
-const int AI4 = A5;
-const int AI5 = A6;
-const int ledPin = 2;
-const int numOfPixels = 6;
-
 int colors[][3] = {
 {255, 0, 0},
 {0, 255, 0},
@@ -51,6 +41,29 @@ int randoms[30][6] = {
 {0,5,2,4,3,1},
 {4,0,3,2,1,5}};
 
+const int numOfPixels = 6;
+
+const int floatingPin = A0;
+const int AI0 = A1;
+const int AI1 = A2;
+const int AI2 = A3;
+const int AI3 = A4;
+const int AI4 = A5;
+const int AI5 = A6;
+const int ledPin = 2;
+const int canPin = 3;
+const int buttonPin = 4;
+const int buttonPin1 = 5;
+const int data0 = 6;
+const int data1 = 7;
+const int data2 = 8;
+const int clk = 9;
+const int enableShootingRange = 1;
+
+int r;
+boolean score[numOfPixels];
+int numCorrect = 0;
+
 Adafruit_NeoPixel leds = Adafruit_NeoPixel(numOfPixels, ledPin, NEO_GRB + NEO_KHZ800);
 
 void setup(){
@@ -61,25 +74,88 @@ void setup(){
   randomSeed(analogRead(floatingPin));
 
   Serial.begin(9600);
+  
+  pinMode(canPin, INPUT_PULLUP);
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(buttonPin1, INPUT_PULLUP);
+  
+  pinMode(data0, OUTPUT);
+  pinMode(data1, OUTPUT);
+  pinMode(data2, OUTPUT);
+  pinMode(clk, OUTPUT);
+  pinMode(enableShottingRange, OUTPUT);
 
 }
 
-
 void loop(){
-  displayRand(500);
-//  rainbow(20);
-  Serial.println(sensorVal(AI0));
-  Serial.println(sensorVal(AI1));
-  Serial.println(sensorVal(AI2));
-  Serial.println(sensorVal(AI3));
-  Serial.println(sensorVal(AI4));
-  Serial.println(sensorVal(AI5));
-  delay(100);
-    
+  while(digitalRead(canPin == LOW) {
+    switch(mode) {
+      case 0: //Display Random Colors
+        numCorrect = 0;
+        r = random(30);
+        displayRand(5000);
+        mode = 1;
+        break;
+      case 1: //Wait for button press
+        if(digitalRead(buttonPin) == LOW) {
+          mode = 2;
+        }
+        break;
+      case 2: //Check sensors and Display pass or fail
+        score[numOfPixels] = checkConnections();
+        for(int i = 0; i < numOfPixels; i++) {
+          leds.setPixelColor(i, colors[bools[i]][0], colors[bools[i]][1], colors[bools[i]][2]);
+        }
+        leds.show();
+        mode = 3;
+        break;
+      case 3: //Transmit data
+        
+        break;
+    } else {
+      rainbow(20);
+    }
+}
+
+boolean[] checkConnections() {
+  int vals[numOfPixels];
+  boolean bools[numOfPixels];
+  vals[0] = sensorVal(AI1);
+  vals[1] = sensorVal(AI2);
+  vals[2] = sensorVal(AI3);
+  vals[3] = sensorVal(AI4);
+  vals[4] = sensorVal(AI5);
+  vals[5] = sensorVal(AI6);
+  for(int i = 0; i < numOfPixels; i++) {
+    if(vals[i] == randoms[r][i]) {
+      bools[i] = 1;
+      numCorrect++;
+    } else {
+      bools[i] = 0;
+    }
+  return bools[];
+}
+  
+int sensorVal(int pin) {
+  int val = analogRead(pin);
+  if(val > 96 && val < 196) {
+    return 0;
+  } else if (val >= 243 && val <= 343) {
+    return 1;
+  } else if (val >= 389 && val <= 489) {
+    return 2;
+  } else if (val >= 535 && val <= 635) {
+    return 3;
+  } else if (val >= 681 && val <= 781) {
+    return 4;
+  } else if (val >= 828 && val <= 928) {
+    return 5;
+  } else {
+    return 6;
+  }
 }
 
 void displayRand(int wait) {
-  int r = random(30);
   for(int i = 0; i < numOfPixels; i++) {
     leds.setPixelColor(i, colors[randoms[r][i]][0], colors[randoms[r][i]][1], colors[randoms[r][i]][2]);
   }
@@ -116,24 +192,6 @@ uint32_t Wheel(byte WheelPos) {
   }
 }
 
-int sensorVal(int pin) {
-  int val = analogRead(pin);
-  if(val > 96 && val < 196) {
-    return 0;
-  } else if (val >= 243 && val <= 343) {
-    return 1;
-  } else if (val >= 389 && val <= 489) {
-    return 2;
-  } else if (val >= 535 && val <= 635) {
-    return 3;
-  } else if (val >= 681 && val <= 781) {
-    return 4;
-  } else if (val >= 828 && val <= 928) {
-    return 5;
-  } else {
-    return 6;
-  }
-}
 
 //
 //
